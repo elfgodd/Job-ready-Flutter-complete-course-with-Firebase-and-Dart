e2c7ddca4aae4287be0832876df54644657d2c60
ElfGod <albertogx@icloud.com>
2022-11-30 22:04:52 -0500

    Static variables and methods part1

---
+++ 01-Intro-To-DartLang/15.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Static variables and methods
+}
+
+class Student {
+  int marks = 0; // Instance variable
+  static int score = 0;
+
+  void increaseMarks() {
+    marks++;
+  }
+}

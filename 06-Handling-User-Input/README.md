83fdbbec8fb5453266b0951b28fca54894f8ae9c
ElfGod <albertogx@icloud.com>
2023-01-04 00:11:37 -0500

    Adding a TextEditing Controller

--- 06-Handling-User-Input/README.md
+++ 06-Handling-User-Input/README.md
@@ -20,4 +20,17 @@ TextField(
 )
 ```
 
+## 2. <u>Adding a TextEditing Controller</u>
 
+### <u>Retrieve the value of a TextField</u>
+
+**Using Controller method:**
+
+1. Create a TextEditingController.
+2. Attach the TextEditingController to a TextField using controller property.
+3. Retrieve the value of the TextField by using the **.text** method provided by the
+   TextEditingController.
+
+`TextEditingController controllerName = TextEditingController();`
+
+`controllerName.text; // It will output the value`
--- 06-Handling-User-Input/flutter_tutorial/lib/homepage.dart
+++ 06-Handling-User-Input/flutter_tutorial/lib/homepage.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // const HomePage({super.key});
+  TextEditingController nameController = TextEditingController();
+  String? name;
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +22,75 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Text(
+                nameController.text,
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal),
+              ),
               TextField(
-                  decoration: InputDecoration(
-                labelText: 'Name',
-                border: OutlineInputBorder(),
-              ))
+                /* onChanged: (value) {
+                  name = value;
+                }, */
+                onChanged: (value) {
+                  setState(() {
+                    name = value;
+                  });
+                },
+                controller: nameController,
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                  print(nameController.text);
+                },
+                child: Text('Print'),
+              )
             ],
           ),
         ));
   }
 }
+
+
+/* import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  // const HomePage({super.key});
+
+  TextEditingController nameController = TextEditingController();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Handler User Input'),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(nameController.text);
+                },
+                child: Text('Print'),
+              )
+            ],
+          ),
+        ));
+  }
+} */
